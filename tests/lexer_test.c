1be37251ba73cf47980de41c04ac97181206ed90
Mitsu325 <patricia.mashiba@gmail.com>
2022-06-29 07:57:19 -0300

    test: ensure check input, heredoc, trunc, append and word

--- tests/lexer_test.c
+++ tests/lexer_test.c
@@ -54,3 +54,147 @@ Test(lexer, pipe_with_simple_quote)
 	free_token_lst(&last_token);
 	ft_matrix_free(cmd);
 }
+
+Test(lexer, is_input)
+{
+	char	**cmd;
+	t_token	*last_token = NULL;
+
+	cmd = malloc(sizeof(char *) * 2);
+	cmd[0] = strdup("<");
+	cmd[1] = NULL;
+	lexer(&last_token, cmd);
+	cr_expect_eq(last_token->value, INPUT, "Ensure return 1 if INPUT");
+	free_token_lst(&last_token);
+	ft_matrix_free(cmd);
+}
+
+Test(lexer, is_not_input)
+{
+	char	**cmd;
+	t_token	*last_token = NULL;
+
+	cmd = malloc(sizeof(char *) * 2);
+	cmd[0] = strdup("   < ");
+	cmd[1] = NULL;
+	lexer(&last_token, cmd);
+	cr_expect_neq(last_token->value, INPUT,
+		"Ensure return not 1 if there are other characters");
+	free_token_lst(&last_token);
+	ft_matrix_free(cmd);
+}
+
+Test(lexer, is_heredoc)
+{
+	char	**cmd;
+	t_token	*last_token = NULL;
+
+	cmd = malloc(sizeof(char *) * 2);
+	cmd[0] = strdup("<<");
+	cmd[1] = NULL;
+	lexer(&last_token, cmd);
+	cr_expect_eq(last_token->value, HEREDOC, "Ensure return 2 if HEREDOC");
+	free_token_lst(&last_token);
+	ft_matrix_free(cmd);
+}
+
+Test(lexer, is_not_heredoc)
+{
+	char	**cmd;
+	t_token	*last_token = NULL;
+
+	cmd = malloc(sizeof(char *) * 2);
+	cmd[0] = strdup("< <");
+	cmd[1] = NULL;
+	lexer(&last_token, cmd);
+	cr_expect_neq(last_token->value, HEREDOC,
+		"Ensure return not 2 if there are other characters");
+	free_token_lst(&last_token);
+	ft_matrix_free(cmd);
+}
+
+Test(lexer, is_trunc)
+{
+	char	**cmd;
+	t_token	*last_token = NULL;
+
+	cmd = malloc(sizeof(char *) * 2);
+	cmd[0] = strdup(">");
+	cmd[1] = NULL;
+	lexer(&last_token, cmd);
+	cr_expect_eq(last_token->value, TRUNC, "Ensure return 3 if TRUNC");
+	free_token_lst(&last_token);
+	ft_matrix_free(cmd);
+}
+
+Test(lexer, is_not_trunc)
+{
+	char	**cmd;
+	t_token	*last_token = NULL;
+
+	cmd = malloc(sizeof(char *) * 2);
+	cmd[0] = strdup(" >");
+	cmd[1] = NULL;
+	lexer(&last_token, cmd);
+	cr_expect_neq(last_token->value, TRUNC,
+		"Ensure return not 3 if there are other characters");
+	free_token_lst(&last_token);
+	ft_matrix_free(cmd);
+}
+
+Test(lexer, is_append)
+{
+	char	**cmd;
+	t_token	*last_token = NULL;
+
+	cmd = malloc(sizeof(char *) * 2);
+	cmd[0] = strdup(">>");
+	cmd[1] = NULL;
+	lexer(&last_token, cmd);
+	cr_expect_eq(last_token->value, APPEND, "Ensure return 4 if APPEND");
+	free_token_lst(&last_token);
+	ft_matrix_free(cmd);
+}
+
+Test(lexer, is_not_append)
+{
+	char	**cmd;
+	t_token	*last_token = NULL;
+
+	cmd = malloc(sizeof(char *) * 2);
+	cmd[0] = strdup("> >");
+	cmd[1] = NULL;
+	lexer(&last_token, cmd);
+	cr_expect_neq(last_token->value, APPEND,
+		"Ensure return not 4 if there are other characters");
+	free_token_lst(&last_token);
+	ft_matrix_free(cmd);
+}
+
+Test(lexer, is_word_2_char)
+{
+	char	**cmd;
+	t_token	*last_token = NULL;
+
+	cmd = malloc(sizeof(char *) * 2);
+	cmd[0] = strdup(" >");
+	cmd[1] = NULL;
+	lexer(&last_token, cmd);
+	cr_expect_eq(last_token->value, WORD, "Ensure return 5 if WORD");
+	free_token_lst(&last_token);
+	ft_matrix_free(cmd);
+}
+
+Test(lexer, is_word_more_2_char)
+{
+	char	**cmd;
+	t_token	*last_token = NULL;
+
+	cmd = malloc(sizeof(char *) * 2);
+	cmd[0] = strdup(" OK ");
+	cmd[1] = NULL;
+	lexer(&last_token, cmd);
+	cr_expect_eq(last_token->value, WORD, "Ensure return 5 if WORD");
+	free_token_lst(&last_token);
+	ft_matrix_free(cmd);
+}
