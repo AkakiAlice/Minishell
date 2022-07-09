bea48eb869804e47b01011a9ccbda02268811a18
Mitsu325 <patricia.mashiba@gmail.com>
2022-07-09 08:33:15 -0300

    test: ensure save variable name

---
+++ tests/save_var_test.c
@@ -0,0 +1,12 @@
+#include <criterion/criterion.h>
+#include "minishell.h"
+
+Test(var, save_var_name)
+{
+	t_env	*last_var = NULL;
+	char	*cmd = {"MINISHELL=/bin/bash"};
+
+	save_var(&last_var, cmd);
+	cr_expect_str_eq(last_var->name, "MINISHELL", "Ensure save variable name");
+	free_env_lst(&last_var);
+}
