938d50c0ab4e6ac70f7384a3c55786331697d2a2
Mitsu325 <patricia.mashiba@gmail.com>
2022-07-09 08:40:36 -0300

    test: ensure save variable value

--- tests/save_var_test.c
+++ tests/save_var_test.c
@@ -10,3 +10,13 @@ Test(var, save_var_name)
 	cr_expect_str_eq(last_var->name, "MINISHELL", "Ensure save variable name");
 	free_env_lst(&last_var);
 }
+
+Test(var, save_var_value)
+{
+	t_env	*last_var = NULL;
+	char	*cmd = {"MINISHELL=/bin/bash"};
+
+	save_var(&last_var, cmd);
+	cr_expect_str_eq(last_var->value, "/bin/bash", "Ensure save variable value");
+	free_env_lst(&last_var);
+}
