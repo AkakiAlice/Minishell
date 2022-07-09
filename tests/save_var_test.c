85c1304b42319f2c3b4fc25f6b596accddb6e0a2
Mitsu325 <patricia.mashiba@gmail.com>
2022-07-09 10:58:39 -0300

    test: ensure variable name validation character and order

--- tests/save_var_test.c
+++ tests/save_var_test.c
@@ -14,9 +14,53 @@ Test(var, save_var_name)
 Test(var, save_var_value)
 {
 	t_env	*last_var = NULL;
-	char	*cmd = {"MINISHELL=/bin/bash"};
+	char	*cmd = {"MINI_SHELL=/bin/bash"};
 
 	save_var(&last_var, cmd);
 	cr_expect_str_eq(last_var->value, "/bin/bash", "Ensure save variable value");
 	free_env_lst(&last_var);
+}
+
+Test(var, validate_var_name_start)
+{
+	t_env	*last_var = NULL;
+	char	*cmd = {"-VAR=/bin/bash"};
+	int		result;
+
+	result = save_var(&last_var, cmd);
+	cr_expect_eq(result, FAILURE,
+	"Ensure dont save variable name and return 1 if failure");
+}
+
+Test(var, validate_var_name_middle)
+{
+	t_env	*last_var = NULL;
+	char	*cmd = {"VAR1*VAR2=/bin/bash"};
+	int		result;
+
+	result = save_var(&last_var, cmd);
+	cr_expect_eq(result, FAILURE,
+	"Ensure dont save variable name and return 1 if failure");
+}
+
+Test(var, validate_var_name_last)
+{
+	t_env	*last_var = NULL;
+	char	*cmd = {"VAR_1!=/bin/bash"};
+	int		result;
+
+	result = save_var(&last_var, cmd);
+	cr_expect_eq(result, FAILURE,
+	"Ensure dont save variable name and return 1 if failure");
+}
+
+Test(var, validate_var_name_first_num)
+{
+	t_env	*last_var = NULL;
+	char	*cmd = {"1_MINISHELL=/bin/bash"};
+	int		result;
+
+	result = save_var(&last_var, cmd);
+	cr_expect_eq(result, FAILURE,
+	"Ensure dont save variable name and return 1 if failure");
 }
