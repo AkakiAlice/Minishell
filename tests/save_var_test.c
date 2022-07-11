4d5c9703dd1e3a0425467204d907af2166967e37
Mitsu325 <patricia.mashiba@gmail.com>
2022-07-11 06:49:58 -0300

    test: change validation quote closed

--- tests/save_var_test.c
+++ tests/save_var_test.c
@@ -305,39 +305,79 @@ Test(count_char, mix_quote)
 Test(validate_quote_closed, single_quote_unclosed)
 {
 	char	*str = {"te''s""t'e"};
-	int		result;
+	bool	result;
 
 	result = validate_quote_closed(str);
-	cr_expect_eq(result, FAILURE,
-		"Ensure returns 1 if the single quotes are unclosed");
+	cr_expect_eq(result, false,
+		"Ensure returns false if the single quotes are open");
 }
 
 Test(validate_quote_closed, single_quote_closed)
 {
 	char	*str = {"te's\"t'e"};
-	int		result;
+	bool	result;
 
 	result = validate_quote_closed(str);
-	cr_expect_eq(result, SUCCESS,
-		"Ensure returns 0 if the single quotes are closed");
+	cr_expect_eq(result, true,
+		"Ensure returns true if the single quotes are closed");
 }
 
 Test(validate_quote_closed, double_quote_unclosed)
 {
 	char	*str = {"te\"\"s't\"e"};
-	int		result;
+	bool	result;
 
 	result = validate_quote_closed(str);
-	cr_expect_eq(result, FAILURE,
-		"Ensure returns 1 if the double quotes are unclosed");
+	cr_expect_eq(result, false,
+		"Ensure returns false if the double quotes are open");
 }
 
 Test(validate_quote_closed, double_quote_closed)
 {
 	char	*str = {"te\"s''t\"e"};
-	int		result;
+	bool	result;
 
 	result = validate_quote_closed(str);
-	cr_expect_eq(result, SUCCESS,
-		"Ensure returns 0 if the double quotes are closed");
+	cr_expect_eq(result, true,
+		"Ensure returns true if the double quotes are closed");
+}
+
+Test(validate_quote_closed, advanced_1)
+{
+	char	*str = {"'\"'\""};
+	bool	result;
+
+	result = validate_quote_closed(str);
+	cr_expect_eq(result, false,
+		"Ensure returns false if the quotes are open");
+}
+
+Test(validate_quote_closed, advanced_2)
+{
+	char	*str = {"ex\"em'plo\"\"\"'"};
+	bool	result;
+
+	result = validate_quote_closed(str);
+	cr_expect_eq(result, false,
+		"Ensure returns false if the quotes are open");
+}
+
+Test(validate_quote_closed, advanced_3)
+{
+	char	*str = {"ex\"em'plo\"\"\"'\""};
+	bool	result;
+
+	result = validate_quote_closed(str);
+	cr_expect_eq(result, false,
+		"Ensure returns false if the quotes are open");
+}
+
+Test(validate_quote_closed, advanced_4)
+{
+	char	*str = {"ex\"o\"'\"d\"'"};
+	bool	result;
+
+	result = validate_quote_closed(str);
+	cr_expect_eq(result, true,
+		"Ensure returns false if the quotes are closed");
 }
