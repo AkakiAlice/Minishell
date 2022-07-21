f7bb08af8943bf72aa7a0bf17f5786e56c09fa19
Mitsu325 <patricia.mashiba@gmail.com>
2022-07-21 06:16:40 -0300

    feat: ensure return 1 if after pipe there is pipe

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 05:24:41 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/07/21 05:47:49 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/07/21 06:14:20 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,24 @@
 
 int	parser(t_token *last_token)
 {
-	if (last_token->next->value == PIPE)
+	t_token	*current;
+
+	if (last_token == NULL)
+		return (0);
+	current = last_token->next;
+	if (current->value == PIPE)
 	{
 		put_msg("minishell", "syntax error near unexpected token `|'", 2);
 		return (1);
+	}
+	while (current != last_token)
+	{
+		if (current->value == PIPE && current->next->value == PIPE)
+		{
+			put_msg("minishell", "syntax error near unexpected token `|'", 2);
+			return (1);
+		}
+		current = current->next;
 	}
 	return (0);
 }
