ea499c51e90f74d9c67a20147cf762405251eb39
Mitsu325 <patricia.mashiba@gmail.com>
2022-07-21 07:25:51 -0300

    feat: add parser

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 20:33:28 by alida-si          #+#    #+#             */
-/*   Updated: 2022/07/21 07:23:43 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/07/21 07:24:47 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ int	main(int argc, char *argv[], char *envp[])
 			tokenizer(&data);
 			last_token = NULL;
 			lexer(&last_token, data.splited_cmdl);
+			parser(last_token);
 			free_token_lst(&last_token);
 			free(data.cmd_line);
 			ft_matrix_free(data.splited_cmdl);
