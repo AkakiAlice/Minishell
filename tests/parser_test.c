8441100e5ddd05e82d5542b0637fc8afc73aa112
Mitsu325 <patricia.mashiba@gmail.com>
2022-07-21 06:22:22 -0300

    test: ensure return 1 if after pipe there is nothing

--- tests/parser_test.c
+++ tests/parser_test.c
@@ -25,3 +25,47 @@ Test(parser, pipe_pipe)
 	cr_expect_eq(result, 1, "Ensure return 1 if after pipe there is pipe");
 	free_token_lst(&last_token);
 }
+
+Test(parser, pipe_word)
+{
+	t_token	*last_token = NULL;
+	int	result;
+
+	token_lst_add_back(&last_token, WORD);
+	token_lst_add_back(&last_token, PIPE);
+	token_lst_add_back(&last_token, WORD);
+	token_lst_add_back(&last_token, PIPE);
+	token_lst_add_back(&last_token, WORD);
+	result = parser(last_token);
+	cr_expect_eq(result, 0, "Ensure return 0 if after pipe there is word");
+	free_token_lst(&last_token);
+}
+
+Test(parser, pipe_redirect)
+{
+	t_token	*last_token = NULL;
+	int	result;
+
+	token_lst_add_back(&last_token, WORD);
+	token_lst_add_back(&last_token, PIPE);
+	token_lst_add_back(&last_token, WORD);
+	token_lst_add_back(&last_token, TRUNC);
+	token_lst_add_back(&last_token, WORD);
+	result = parser(last_token);
+	cr_expect_eq(result, 0, "Ensure return 0 if after pipe there is redirect");
+	free_token_lst(&last_token);
+}
+
+Test(parser, pipe_empty)
+{
+	t_token	*last_token = NULL;
+	int	result;
+
+	token_lst_add_back(&last_token, WORD);
+	token_lst_add_back(&last_token, PIPE);
+	token_lst_add_back(&last_token, WORD);
+	token_lst_add_back(&last_token, PIPE);
+	result = parser(last_token);
+	cr_expect_eq(result, 1, "Ensure return 1 if after pipe there is nothing");
+	free_token_lst(&last_token);
+}
