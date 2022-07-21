1e31718b8b3bcceae99d59a3138624bba29a04b4
Mitsu325 <patricia.mashiba@gmail.com>
2022-07-21 06:16:47 -0300

    test: ensure return 1 if after pipe there is pipe

--- tests/parser_test.c
+++ tests/parser_test.c
@@ -11,3 +11,17 @@ Test(parser, only_pipe)
 	cr_expect_eq(result, 1, "Ensure return 1 if you only have one pipe");
 	free_token_lst(&last_token);
 }
+
+Test(parser, pipe_pipe)
+{
+	t_token	*last_token = NULL;
+	int	result;
+
+	token_lst_add_back(&last_token, WORD);
+	token_lst_add_back(&last_token, PIPE);
+	token_lst_add_back(&last_token, PIPE);
+	token_lst_add_back(&last_token, WORD);
+	result = parser(last_token);
+	cr_expect_eq(result, 1, "Ensure return 1 if after pipe there is pipe");
+	free_token_lst(&last_token);
+}
