f1b1205b58b2be20cb8fc721f80a736633861d79
Alice Akaki <akakialice@gmail.com>
2022-07-22 16:53:40 -0400

    feat: handling the ./cmd entry on the command line

--- src/exec/exec_cmd.c
+++ src/exec/exec_cmd.c
@@ -6,11 +6,50 @@
 /*   By: alida-si <alida-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 23:52:12 by alida-si          #+#    #+#             */
-/*   Updated: 2022/07/21 19:41:48 by alida-si         ###   ########.fr       */
+/*   Updated: 2022/07/22 16:47:17 by alida-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/*	NO_SUCH_FILE_EXIT
+**	------------
+**	DESCRIPTION
+**	Displays the error message "Is a directory" and terminates the process.
+**	PARAMETERS
+**	#1. The pointer to struct "data" (data);
+**	#2. The pointer to list (last_env);
+**	RETURN VALUES
+**	-
+*/
+void	is_dir_exit(t_data *data, t_env **last_env)
+{
+	write(2, data->splited_cmdl[0], ft_strlen(data->splited_cmdl[0]));
+	write(2, ": Is a directory\n", 17);
+	ft_matrix_free(data->splited_cmdl);
+	free_env_lst(last_env);
+	exit(126);
+}
+
+/*	NO_SUCH_FILE_EXIT
+**	------------
+**	DESCRIPTION
+**	Displays the error message "No such file or directory"
+**	and terminates the process.
+**	PARAMETERS
+**	#1. The pointer to struct "data" (data);
+**	#2. The pointer to list (last_env);
+**	RETURN VALUES
+**	-
+*/
+void	no_such_file_exit(t_data *data, t_env **last_env)
+{
+	write(2, data->splited_cmdl[0], ft_strlen(data->splited_cmdl[0]));
+	write(2, ": No such file or directory\n", 28);
+	ft_matrix_free(data->splited_cmdl);
+	free_env_lst(last_env);
+	exit(127);
+}
 
 /*	CHECK_IS_DIR
 **	------------
@@ -29,20 +68,13 @@ void	check_is_dir(t_data *data, t_env **last_env)
 	dir = opendir(data->splited_cmdl[0]);
 	if (dir)
 	{
-		write(2, data->splited_cmdl[0], ft_strlen(data->splited_cmdl[0]));
-		write(2, ": Is a directory\n", 17);
 		closedir(dir);
-		ft_matrix_free(data->splited_cmdl);
-		free_env_lst(last_env);
-		exit(126);
+		is_dir_exit(data, last_env);
 	}
-	else if (ENOENT == errno && data->cmd_path == NULL)
+	else if ((ENOENT == errno && data->cmd_path == NULL)
+		|| (access(data->splited_cmdl[0], X_OK) == -1))
 	{
-		write(2, data->splited_cmdl[0], ft_strlen(data->splited_cmdl[0]));
-		write(2, ": No such file or directory\n", 28);
-		ft_matrix_free(data->splited_cmdl);
-		free_env_lst(last_env);
-		exit(127);
+		no_such_file_exit(data, last_env);
 	}
 	else if ((access(data->splited_cmdl[0], X_OK) == 0))
 		data->cmd_path = data->splited_cmdl[0];
@@ -60,8 +92,6 @@ void	check_is_dir(t_data *data, t_env **last_env)
 */
 void	exec_cmd(t_data *data, t_env **last_env)
 {
-	if (data->splited_cmdl[0][0] == '/')
-		check_is_dir(data, last_env);
 	if (data->cmd_path == NULL)
 	{
 		write(2, data->splited_cmdl[0], ft_strlen(data->splited_cmdl[0]));
@@ -90,7 +120,11 @@ void	fork_it(t_data *data, t_env **last_env)
 
 	pid = fork();
 	if (pid == 0)
+	{
+		if (ft_strchr(data->splited_cmdl[0], '/') != NULL)
+			check_is_dir(data, last_env);
 		exec_cmd(data, last_env);
+	}
 	waitpid(0, &data->status, 0);
 	if (WIFEXITED(data->status))
 		p_status = WEXITSTATUS(data->status);
