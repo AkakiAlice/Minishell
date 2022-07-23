59799bbb36030ed3f3a3d60c0a91de2950de883b
Alice Akaki <akakialice@gmail.com>
2022-07-23 15:40:03 -0400

    feat: expansion exit code

--- src/main.c
+++ src/main.c
@@ -6,11 +6,20 @@
 /*   By: alida-si <alida-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 20:33:28 by alida-si          #+#    #+#             */
-/*   Updated: 2022/07/23 14:45:53 by alida-si         ###   ########.fr       */
+/*   Updated: 2022/07/23 15:38:41 by alida-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	put_exit_code(t_data *data)
+{
+	if (ft_strchr(data->cmd_line, '$') != NULL)
+	{
+		free(data->splited_cmdl[1]);
+		data->splited_cmdl[1] = ft_strdup(ft_itoa(data->status));
+	}
+}
 
 /*	RUN_CMD
 **	------------
@@ -25,6 +34,7 @@
 void	run_cmd(t_data *data, char	*path_value)
 {
 	tokenizer(data);
+	put_exit_code(data);
 	builtin(data);
 	lexer(&data->last_token, data->splited_cmdl);
 	parser(data->last_token);
@@ -38,8 +48,9 @@ int	main(int argc, char *argv[], char *envp[])
 	char	*path_value;
 
 	(void)argv;
-	if (argc > 1)
-		error_msg_exit("minishell", TOO_MANY_ARG, 2);
+	(void)argc;
+	/*if (argc > 1)
+		error_msg_exit("minishell", TOO_MANY_ARG, 2);*/
 	minishell_init(&data);
 	save_env(&data.last_env, envp);
 	path_value = get_path(data.last_env);
