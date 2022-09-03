4a5f856f30c4e7cb09873460cc50cccc8381a85e
Alice Akaki <akakialice@gmail.com>
2022-09-03 13:12:08 -0400

    feat: unset error msg

--- src/builtins/builtin_unset.c
+++ src/builtins/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: alida-si <alida-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 22:04:34 by alida-si          #+#    #+#             */
-/*   Updated: 2022/09/02 14:01:04 by alida-si         ###   ########.fr       */
+/*   Updated: 2022/09/03 13:05:44 by alida-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,15 +51,43 @@ void	unset_env_var(t_env **env_list, char *word)
 		del_node(env_list, word);
 }
 
+int	valid_unset_var(char *word)
+{
+	int	i;
+
+	i = 0;
+	if (ft_isalpha(word[i]) != 0 || word[i] == '_')
+	{
+		while (word[i] != '\0')
+		{
+			if (ft_isalnum(word[i]) == 0 && word[i] != '_')
+				return (0);
+			i++;
+		}
+		return (1);
+	}
+	return(0);
+}
+
+void	unset_error_msg(char *word, t_data *data)
+{
+	ft_putstr_fd("-minishell: unset: `", 2);
+	ft_putstr_fd(word, 2);
+	ft_putstr_fd("\': not a valid identifier\n", 2);
+	data->status = 1;
+}
+
 void	builtin_unset(t_data *data, t_cmdtable *cmd_table)
 {
 	int		i;
-	t_env	*temp;
 
 	i = 1;
 	while (cmd_table->word[i] != NULL)
 	{
-		unset_env_var(&data->head_env, cmd_table->word[i]);
+		if (valid_unset_var(cmd_table->word[i]))
+			unset_env_var(&data->head_env, cmd_table->word[i]);
+		else
+			unset_error_msg(cmd_table->word[i], data);
 		i++;
 	}
 }
