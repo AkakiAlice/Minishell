8de15f84f7c31643af6cd10095b2fce34dac118f
Alice Akaki <akakialice@gmail.com>
2022-09-07 14:35:27 -0400

    refactor: builtin cd

--- src/builtins/builtin_cd.c
+++ src/builtins/builtin_cd.c
@@ -6,11 +6,19 @@
 /*   By: alida-si <alida-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 12:54:06 by alida-si          #+#    #+#             */
-/*   Updated: 2022/09/07 14:19:07 by alida-si         ###   ########.fr       */
+/*   Updated: 2022/09/07 14:33:06 by alida-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	error_msg_cd(char *str, char *msg)
+{
+	ft_putstr_fd("-minishell: ", 2);
+	ft_putstr_fd("cd: ", 2);
+	put_msg(str, msg, 2);
+	g_data.status = 1;
+}
 
 int	check_is_dir_cd(t_cmdtable *cmd_table)
 {
@@ -19,18 +27,12 @@ int	check_is_dir_cd(t_cmdtable *cmd_table)
 	dir = opendir(cmd_table->word[1]);
 	if (!dir && access(cmd_table->word[1], F_OK) == -1)
 	{
-		ft_putstr_fd("-minishell: ", 2);
-		ft_putstr_fd("cd: ", 2);
-		put_msg(cmd_table->word[1], NO_FILE_DIR, 2);
-		g_data.status = 1;
+		error_msg_cd(cmd_table->word[1], NO_FILE_DIR);
 		return (0);
 	}
 	if (access(cmd_table->word[1], F_OK) == 0 && !dir)
 	{
-		ft_putstr_fd("-minishell: ", 2);
-		ft_putstr_fd("cd: ", 2);
-		put_msg(cmd_table->word[1], NO_DIR, 2);
-		g_data.status = 1;
+		error_msg_cd(cmd_table->word[1], NO_DIR);
 		return (0);
 	}
 	closedir(dir);
@@ -53,6 +55,23 @@ void	change_dir(t_data *data, char *dir)
 	free(old_pwd);
 }
 
+void	change_dir_old_pwd(t_data *data)
+{
+	char	*old_pwd;
+
+	old_pwd = get_var_value_expand(data->head_env, "OLDPWD");
+	if (old_pwd == NULL)
+	{
+		ft_putstr_fd("-minishell: cd: OLDPWD not set\n", 2);
+		g_data.status = 1;
+	}
+	else
+	{
+		ft_printf("%s\n", old_pwd);
+		change_dir(data, old_pwd);
+	}
+}
+
 void	builtin_cd(t_data *data, t_cmdtable *cmd_table)
 {
 	if (cmd_table->word[1] && cmd_table->word[2])
@@ -62,15 +81,7 @@ void	builtin_cd(t_data *data, t_cmdtable *cmd_table)
 		return ;
 	}
 	if (strcmp_eq(cmd_table->word[1], "-"))
-	{
-		if (get_var_value_expand(data->head_env, "OLDPWD") == NULL)
-			ft_putstr_fd("-minishell: cd: OLDPWD not set\n", 2);
-		else
-		{
-			ft_printf("%s\n", get_var_value_expand(data->head_env, "OLDPWD"));
-			change_dir(data, get_var_value_expand(data->head_env, "OLDPWD"));
-		}
-	}
+		change_dir_old_pwd(data);
 	else if (cmd_table->word[1] == NULL || strcmp_eq(cmd_table->word[1], "~")
 		|| strcmp_eq(cmd_table->word[1], "~/"))
 		change_dir(data, getenv("HOME"));
